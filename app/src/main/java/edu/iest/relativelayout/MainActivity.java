7b1283d1240184773f9eacdf18e9fe583eda90a4
ISND 8vo ISND89 9no Semestre <ISND89@AISND20-04.campus.iest.edu.mx>
2023-02-17 18:09:26 -0600

    Clase del 17 de Feb

--- app/src/main/java/edu/iest/relativelayout/MainActivity.java
+++ app/src/main/java/edu/iest/relativelayout/MainActivity.java
@@ -3,12 +3,61 @@ package edu.iest.relativelayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bnCanal = findViewById(R.id.boton);
+        EditText etCanal = findViewById(R.id.etCanal);
+        TextView tvCanal = findViewById(R.id.tvCanal);
+        Spinner spProgramas = findViewById(R.id.programa);
+        ivContinuara = findViewById(R.id.ivContinuara);
+
+        spProgramas.setOnItemSelectedListener(this);
+
+        bnCanal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //aqui la logica que hacer al dar clic
+                //obtendremos el valor del edittext y lo asignaremos al textview inferior
+
+                String texto = etCanal.getText().toString();
+                Toast.makeText(MainActivity.this,"El valor era "+texto, Toast.LENGTH_LONG).show();
+                tvCanal.setText(texto);
+            }
+        });
+    }
+    private ImageView ivContinuara;
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int posicion, long l) {
+        String programa = adapterView.getItemAtPosition(posicion).toString();
+        Snackbar.make(adapterView,"Programa seleccionado "+programa,Snackbar.LENGTH_LONG).show();
+
+        if(posicion == 0){
+            ivContinuara.setImageResource(R.drawable.tobecontinued_onepiece);
+        }else if (posicion == 1){
+            ivContinuara.setImageResource(R.drawable.tobecontinued_jojos);
+        }else {
+            ivContinuara.setImageResource(R.drawable.tobecontinued_looneytunes);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
